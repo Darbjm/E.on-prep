896a125d69049f4b56abbe992163bd0e008bd47a
james.michael.darby@gmail.com <james.darby@fdmgroup.com>
2021-04-07 11:02:43 +0100

    django setup

--- threading_multiprocessing.py
+++ threading_multiprocessing.py
@@ -34,3 +34,107 @@
 # - Use a different, free-threaded Python implementation (Jython, IronPython)
 # - User Python as a wrapper for third-party lbraries (C/C++) -> numpy, scipy
 
+from threading import Thread, Lock
+import time
+
+# def square_numbers():
+#     for i in range(100):
+#         i + i
+#         time.sleep(0.1)
+
+# threads = []
+# num_threads = 10
+
+# # create threads
+# for i in range(num_threads):
+#     t = Thread(target=square_numbers)
+#     threads.append(t)
+
+# # start
+# for t in threads:
+#     t.start()
+
+# #join
+# for t in threads:
+#     t.join()
+
+# print('end main')
+
+database_value = 0
+
+def increase(lock):
+    global database_value
+
+    lock.acquire()
+    local_copy = database_value
+    local_copy += 1
+    time.sleep(0.1)
+    database_value = local_copy
+    lock.release()
+
+if __name__ == '__main__':
+
+    lock = Lock()
+    print('start value', database_value)
+
+    thread1 = Thread(target=increase, args=(lock,))
+    thread2 = Thread(target=increase, args=(lock,))
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+    print('end value', database_value)
+
+    print('end main')
+
+############################# PROCESSING
+
+# from multiprocessing import Process
+# import time
+# import os
+
+# def process():
+#     def square_numbers():
+#         for i in range(100):
+#             i + i
+#             time.sleep(0.1)
+
+#     processes = []
+#     num_processes = os.cpu_count()
+
+#     # create threads
+#     for i in range(num_processes):
+#         p = Process(target=square_numbers)
+#         processes.append(p)
+
+#     # start
+#     for p in processes:
+#         p.start()
+
+    #join
+    # for p in processes:
+        # p.join()
+
+    # print('end main')
+
+# if __name__ == '__main__':
+    # freeze_support()
+    # process()
+
+
+# import multiprocessing as mp  
+# import time  
+
+# def test_function(i):   
+#     print("function starts" + " " + str(i))  
+#     time.sleep(1)  
+#     print("function ends" + str(i))  
+
+# if __name__ == '__main__':  
+#     pool = mp.Pool(mp.cpu_count())  
+#     pool.map(test_function, [i for i in range(4)])  
+#     pool.close()  
+#     pool.join()  
