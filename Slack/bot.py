e4bffa927d2618da10f2781caa95e70b17b66daf
Darbjm <james.michael.darby@gmail.com>
2021-04-12 16:16:12 +0100

    bot is up

--- Slack/bot.py
+++ Slack/bot.py
@@ -1,4 +1,5 @@
-import slack
+from slack import WebClient
+from slack.errors import SlackApiError
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -6,4 +7,15 @@ from dotenv import load_dotenv
 env_path = Path('.')/'env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(toekn=os.environ['SLACK_TOKEN'])
+client = WebClient(token=os.environ['SLACK_TOKEN'])
+
+try:
+    client.conversations_join(channel="C01U5JCV6EQ")
+    response = client.chat_postMessage(
+        channel='#general',
+        text="Hello world!")
+    assert response["message"]["text"] == "Hello world!"
+except SlackApiError as e:
+    assert e.response["ok"] is False
+    assert e.response["error"]
+    print(f"Got an error: {e.response['error']}")
